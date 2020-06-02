70f91af6d2da6506eef50b053722eee6a3518f4f
Omaru Jawara <omaru.jawara94@gmail.com>
2020-06-02 16:44:08 -0400

    initial commit

---
+++ FirstApp/practice-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+class HelloWorld extends Component {
+  render() {
+    return <h1>Hello Worlds!</h1>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return <HelloWorld />;
+  }
+}
+
+export default App;
